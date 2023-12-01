4c762380c0b5efdf042ef237b0d85c8250b5e942
1r-1na <irij666@gmail.com>
2023-12-01 16:46:59 +0100

    1.2

--- day_1/main.py
+++ day_1/main.py
@@ -1,12 +1,22 @@
 data = open('./data.txt').read()
 
-sum = 0
-for line in data.splitlines():
-    digit = ''
-    for character in line:
-        if(character.isdigit()):
-            digit += character
-    digit = int(digit[0] + digit[-1])
-    sum += digit
+def calibrate(input):
+    sum = 0
+    for line in input.splitlines():
+        digit = ''
+        for character in line:
+            if(character.isdigit()):
+                digit += character
+        digit = int(digit[0] + digit[-1])
+        sum += digit
+    return sum
 
-print(sum)
+
+result = calibrate(data)
+print("First result:", result)
+
+data_mapped = data.replace('one', 'o1e').replace('two', 't2o').replace('three', 't3e').replace('four', 'f4r').replace('five', 'f5e').replace('six', 's6x').replace('seven', 's7n').replace('eight', 'e8t').replace('nine', 'n9e')
+
+result_corrected = calibrate(data_mapped)
+print("Second result:", result_corrected)
+
