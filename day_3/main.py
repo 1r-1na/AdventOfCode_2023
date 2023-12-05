8caa09fd318a1fbeb269862e5cd662b9668eb364
1r-1na <irij666@gmail.com>
2023-12-05 17:24:33 +0100

    3.1

--- day_3/main.py
+++ day_3/main.py
@@ -1,76 +1,46 @@
-import numpy as np
+data = open('./data.txt').readlines()
 
-data = open('./day_3/data.txt').readlines()
 
-# lines: [ [......458....#],
-#          [....456+.....#],
-#          [......#.893...] ]
-#
-# number_positions: [6, 7, 8]
-#
-def is_part_number(number_positions: list, lines: np.array, isFirstLine = False, isLastLine = False):
-    current_line_index = 1
-    if isFirstLine:
-        current_line_index = 0
-    line_length = len(lines[current_line_index])
-    # check left side
-    if number_positions[0] > 0:
-        if lines[current_line_index][number_positions[0] - 1] != '.' and not lines[current_line_index][number_positions[0] - 1].isdigit():
+def is_part_number(positions_to_check: list, lines: list, on_left: bool, on_right: bool):
+    if not on_left:
+        positions_to_check.insert(0, positions_to_check[0] - 1)
+    if not on_right:
+        positions_to_check.append(positions_to_check[-1] + 1)
+    for li in lines:
+        characters_to_check = li[positions_to_check[0]:positions_to_check[-1] + 1]
+        if any(not c.isdigit() and not c == '.' for c in characters_to_check):
             return True
-    # check right side
-    if number_positions[-1] < line_length - 1:
-        if lines[current_line_index][number_positions[-1] + 1] != '.' and not lines[current_line_index][number_positions[-1] + 1].isdigit():
-            return True
-    # check above
-    if not isFirstLine:
-        for num_pos in number_positions:
-            if lines[current_line_index - 1][num_pos] != '.' and not lines[current_line_index - 1][num_pos].isdigit():
-                return True
-        # check left diagonal
-        if number_positions[0] > 0:
-            if lines[current_line_index - 1][number_positions[0] - 1] != '.' and not lines[current_line_index - 1][number_positions[0] - 1].isdigit():
-                return True
-        # check right diagonal
-        if number_positions[-1] < line_length - 1:
-            if lines[current_line_index - 1][number_positions[-1] + 1] != '.' and not lines[current_line_index - 1][number_positions[-1] + 1].isdigit():
-                return True
-    # check below
-    if not isLastLine:
-        for num_pos in number_positions:
-            if lines[current_line_index + 1][num_pos] != '.' and not lines[current_line_index + 1][num_pos].isdigit():
-                return True
-        # check left diagonal
-        if number_positions[0] > 0:
-            if lines[current_line_index + 1][number_positions[0] - 1] != '.' and not lines[current_line_index + 1][number_positions[0] - 1].isdigit():
-                return True
-        # check right diagonal
-        if number_positions[-1] < line_length - 1:
-            if lines[current_line_index + 1][number_positions[-1] + 1] != '.' and not lines[current_line_index + 1][number_positions[-1] + 1].isdigit():
-                return True
     return False
+
 
 part_numbers_sum = 0
 
-# index for a line
-for i, line in enumerate(data):
-    positions = []
-    number_detected = False
-    for j, character in enumerate(line):
-        if number_detected:
-            part_number = False
-            if i == 0:
-                part_number = is_part_number(positions, np.array([line, data[i + 1]]), isFirstLine=True)
-            elif i == len(data) - 1:
-                part_number = is_part_number(positions, np.array([data[i - 1], line]), isLastLine=True)
-            else:
-                part_number = is_part_number(positions, np.array([data[i - 1], line, data[i + 1]]))
-            if part_number:
-                part_numbers_sum += int(line[positions[0]:(positions[-1]+1)])
-            number_detected = False
-            positions = []
+positions = []
+last_index_data = len(data) - 1
+
+for line_index, line in enumerate(data):
+    last_line_index = len(line) - 1
+    for char_index, character in enumerate(line):
+        if char_index == last_line_index:
+            continue
+
         if character.isdigit():
-            positions.append(j)
-        elif len(positions) > 0:
-           number_detected = True
+            positions.append(char_index)
+
+        if (line[char_index + 1] == '\n' or not line[char_index + 1].isdigit()) and len(positions) > 0:
+            # number detected
+            lines_to_check = []
+            if line_index != 0:
+                lines_to_check.append(data[line_index - 1])
+            lines_to_check.append(line)
+            if line_index != last_index_data:
+                lines_to_check.append(data[line_index + 1])
+
+            if is_part_number(positions.copy(), lines_to_check, on_left=(positions[0] == 0),
+                              on_right=(line[char_index + 1] == '\n')):
+                # part number detected
+                n = line[positions[0]:positions[-1] + 1]
+                part_numbers_sum += int(n)
+            positions.clear()
 
 print("Sum of all part numbers:", part_numbers_sum)
