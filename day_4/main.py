f169f20f51517e9e8e7f85c96833e67e8ce082cf
1r-1na <irij666@gmail.com>
2023-12-07 09:58:31 +0100

    4.2

--- day_4/main.py
+++ day_4/main.py
@@ -3,8 +3,8 @@ import re
 data = open('./data.txt').readlines()
 
 
-def parse_line(l: str):
-    line_split = l.split('|')
+def parse_line(li: str):
+    line_split = li.split('|')
 
     x = re.sub(r'^Card.*:', '', line_split[0], 1).split(' ')
     x = filter(lambda n: n != '', x)
@@ -17,16 +17,47 @@ def parse_line(l: str):
     return winning_numbers, actual_numbers
 
 
-total = 0
-for line in data:
-    point = 0
-    wn, an = parse_line(line)
-    for current_num in an:
-        if current_num in wn:
-            if point > 0:
-                point += point
-            else:
-                point = 1
-    total += point
+def part_1():
+    total = 0
+    for line in data:
+        point = 0
+        wn, an = parse_line(line)
+        for current_num in an:
+            if current_num in wn:
+                if point > 0:
+                    point += point
+                else:
+                    point = 1
+        total += point
+    return total
 
-print("Points worth in total:", total) # 26346
+
+def part_2():
+    collected_cards = {}
+    last_index = len(data) - 1
+    for index, line in enumerate(data):
+        wn, an = parse_line(line)
+        # add original card
+        if index in collected_cards:
+            collected_cards[index] = collected_cards[index] + 1
+        else:
+            collected_cards[index] = 1
+        count_cards_at_index = collected_cards[index]
+        count_matching_numbers = 0
+        for actual_number in an:
+            if actual_number in wn:
+                count_matching_numbers += 1
+        for i in range(count_matching_numbers):
+            next_index = index + i + 1
+            if next_index > last_index:
+                break
+            if next_index in collected_cards:
+                collected_cards[next_index] = collected_cards[next_index] + count_cards_at_index
+            else:
+                collected_cards[next_index] = count_cards_at_index
+
+    return sum(collected_cards.values())
+
+
+print("Points worth in total:", part_1())  # 26346
+print("Total scratchcards:", part_2())  # 8467762
